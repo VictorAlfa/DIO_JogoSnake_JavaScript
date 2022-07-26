a4d3a63cb9a4ba84bb571d99aff0e60b19474e42
VictorAlfa <victorosantos@yahoo.com.br>
2022-07-25 22:15:56 -0300

    CriarSnake

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ function load(){
 let canvas=document.getElementById("snake");
 let context=canvas.getContext("2d");
 let box=32;
+let snake=[];
+
+snake[0]={
+    x:8*box,
+    y:8*box
+}
 
 //Criando BackGround
 function criarBG(){
@@ -14,4 +20,12 @@ function criarBG(){
     context.fillRect(0 , 0 , 16*box , 16*box);
 }
 
+function criarSnake(){
+    for(i=0 ; snake.length ; i++){
+        context.fillStyle="green";
+        context.fillRect(snake[i].x , snake[i].y , box , box);
+    }
+}
+
 criarBG();
+criarSnake();
